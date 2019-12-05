4bd0119e78349bef99f0a9adfaa25c30d997798f
fuston05 <fuston05@yahoo.com>
2019-12-04 23:47:05 -0500

    tinkering trying to get timer hooked up to start button. button still not working atm

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,70 +1,76 @@
-let msTensCount= 0;
-let msHundredsCount= 0;
-let secondsOnesCount= 0;
-let secondsTensCount= 0;
+function startTimer(){
 
-const interval= window.setInterval( function(){
-    //get msTens
-    const msTens= document.querySelector('#msTens');
+    //initial count vars
+    let msTensCount= 0;
+    let msHundredsCount= 0;
+    let secondsOnesCount= 0;
+    let secondsTensCount= 0;
 
-    //get msHunderds
-    const msHundreds= document.querySelector('#msHundreds');
+    const interval= window.setInterval( function(){ //set the timer
+        //get msTens
+        const msTens= document.querySelector('#msTens');
 
-    //get secondsOnes
-    const secondsOnes= document.querySelector('#secondsOnes');
+        //get msHunderds
+        const msHundreds= document.querySelector('#msHundreds');
 
-    //get secondsTens
-    const secondsTens= document.querySelector('#secondsTens');
+        //get secondsOnes
+        const secondsOnes= document.querySelector('#secondsOnes');
 
-    //get digits as a nodeList
-    const getDigits= document.querySelectorAll('.digit');
+        //get secondsTens
+        const secondsTens= document.querySelector('#secondsTens');
 
-    // set initial '0' for all
-    msTens.textContent= msTensCount;
-    msHundreds.textContent= msHundredsCount;
-    secondOnes.textContent= secondsOnesCount;
-    secondTens.textContent= secondsTensCount;
+        //get digits as a nodeList
+        const getDigits= document.querySelectorAll('.digit');
 
-    //start incrementing from smallest digit up
-    msTens.textContent= msTensCount;
-    msTensCount++;
-    
-    if(msTensCount > 9){ // increment msHundreds when tens > 9
-        msHundreds.textContent= msHundredsCount;
-        msHundredsCount ++;
-        msTensCount= 0;
-    }//end if msTens
-        
-    if( msHundredsCount > 9 ){ //increment secondOnes when msHundreds > 9
-        msHundredsCount= 0;
-        secondOnes.textContent= secondsOnesCount;
-        secondsOnesCount++;
-    }//end if msHundreds
-
-    if( secondsOnesCount > 9 ){ //increment secondTens when secondOnes > 9
-        secondTens.textContent= secondsTensCount;
-        secondsOnesCount= 0;
-        secondsTensCount++;
-    }// end if
-
-    if( secondsTensCount ===1 ){ //when 10 sec, update all digits, stop timer, change color
+        // set initial '0' for all
         msTens.textContent= msTensCount;
         msHundreds.textContent= msHundredsCount;
         secondOnes.textContent= secondsOnesCount;
         secondTens.textContent= secondsTensCount;
-        clearInterval(interval);//stop timer
 
-        //change digit color 'nodeList'
-        for(let i=0; i < getDigits.length; i++){
-            getDigits[i].style.color= 'red';
-        }//end for
+        //start incrementing from smallest digit up
+        msTens.textContent= msTensCount;
+        msTensCount++;
+        
+        if(msTensCount > 9){ // increment msHundreds when tens > 9
+            msHundreds.textContent= msHundredsCount;
+            msHundredsCount ++;
+            msTensCount= 0;
+        }//end if msTens
+            
+        if( msHundredsCount > 9 ){ //increment secondOnes when msHundreds > 9
+            msHundredsCount= 0;
+            secondOnes.textContent= secondsOnesCount;
+            secondsOnesCount++;
+        }//end if msHundreds
 
-    }// end if
-}, 10 );//end interval
+        if( secondsOnesCount > 9 ){ //increment secondTens when secondOnes > 9
+            secondTens.textContent= secondsTensCount;
+            secondsOnesCount= 0;
+            secondsTensCount++;
+        }// end if
+
+        if( secondsTensCount ===1 ){ //when 10 sec, update all digits, stop timer, change color
+            msTens.textContent= msTensCount;
+            msHundreds.textContent= msHundredsCount;
+            secondOnes.textContent= secondsOnesCount;
+            secondTens.textContent= secondsTensCount;
+            clearInterval(interval);//stop timer
+
+            //change digit color 'nodeList'
+            for(let i=0; i < getDigits.length; i++){
+                getDigits[i].style.color= 'red';
+            }//end for
+
+        }// end if
+    }, 10 );//end interval
+
+}//end func
 
 
-/* <div class="digit" id="secondTens">-</div>
-<div class="digit" id="secondOnes">-</div>
-<div class="digit" id="colon">:</div>
-<div class="digit" id="msHundreds">-</div>
-<div class="digit" id="msTens">-</div> */
+//get button
+const startBtn= document.querySelector('.button');
+
+startBtn.addEventListener('click', (event) =>{
+    alert('working');
+} );
