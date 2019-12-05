8e32af7c03e0efb4706e64fb56ea81103c1eab77
fuston05 <fuston05@yahoo.com>
2019-12-04 23:09:55 -0500

    needs some major refactoring, but the stretch timer is done

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,7 +1,12 @@
+let msTensCount= 0;
+let msHundredsCount= 0;
+let secondsOnesCount= 0;
+let secondsTensCount= 0;
 
 const interval= window.setInterval( function(){
     
     const max= 10;
+    
     //get msTens
     const msTens= document.querySelector('#msTens');
 
@@ -14,16 +19,50 @@ const interval= window.setInterval( function(){
     //get secondsTens
     const secondsTens= document.querySelector('#secondsTens');
 
+    const getDigits= document.querySelectorAll('.digit');
 
-    for(let i= 1; i <= max; i++){
-        if( i === 10 ){
-            clearInterval(interval);
-        }//end if
+    // set initial '0' for all
+    msTens.textContent= msTensCount;
+    msHundreds.textContent= msHundredsCount;
+    secondOnes.textContent= secondsOnesCount;
+    secondTens.textContent= secondsTensCount;
 
-        msTens.textContent
 
-    }//end for
+    msTens.textContent= msTensCount;
+    msTensCount++;
+    
+    if(msTensCount > 9){
 
+        msHundreds.textContent= msHundredsCount;
+        msHundredsCount ++;
+        msTensCount= 0;
+    }//end if msTens
+        
+    if( msHundredsCount > 9 ){
+        msHundredsCount= 0;
+        secondOnes.textContent= secondsOnesCount;
+        secondsOnesCount++;
+    }//end if msHundreds
+
+    if( secondsOnesCount > 9 ){
+        secondTens.textContent= secondsTensCount;
+        secondsOnesCount= 0;
+        secondsTensCount++;
+    }// end if
+
+    if( secondsTensCount ===1 ){
+        msTens.textContent= msTensCount;
+        msHundreds.textContent= msHundredsCount;
+        secondOnes.textContent= secondsOnesCount;
+        secondTens.textContent= secondsTensCount;
+        clearInterval(interval);//stop timer
+
+        //change digit color 'nodeList'
+        for(let i=0; i < getDigits.length; i++){
+            getDigits[i].style.color= 'red';
+        }//end for
+
+    }// end if
 }, 10 );//end interval
 
 
